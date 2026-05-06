476ba4450ea8debfe15513650748b32be57631df
claudiasakura69-droid <claudiasakura69@gmail.com>
2026-05-06 13:46:59 +0700

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
-  title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
+  title: 'Yoii',
+  github: 'https://github.com/',
+  coffee: 'https://yoii.me/coffee',
+  twitter: 'https://yoii.me/x',
+  telegram: 'https://yoii.me/telegram',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://yoii.me/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
