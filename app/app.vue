fba747098cfb4e1cfd2385e36bff83f9c2dd26c6
claudiasakura69-droid <claudiasakura69@gmail.com>
2026-05-06 13:42:52 +0700

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -26,11 +26,15 @@ useHead(() => ({
       name: 'viewport',
       content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
     },
+    {
+      name: 'robots',
+      content: 'noindex, nofollow',
+    },
   ],
   link: [
     {
       rel: 'canonical',
-      href: computed(() => `https://sink.cool${route.path}`),
+      href: computed(() => `https://yoii.me${route.path}`),
     },
     {
       rel: 'icon',
